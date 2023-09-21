a083a6e9c017ab5de2aecf58f1e81fb8c1b4cee4
tbarracha <taigo.barracha@gmail.com>
2023-09-21 13:38:03 +0100

    Added Flax Event

--- Source/Game/Scripts/Utilities/FlaxEvent.cs
+++ Source/Game/Scripts/Utilities/FlaxEvent.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event
+public class FlaxEvent
 {
     private event Action action = delegate { };
 
@@ -41,10 +40,9 @@ public class Event
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T>
+public class FlaxEvent<T>
 {
     private event Action<T> action = delegate { };
 
@@ -69,10 +67,9 @@ public class Event<T>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2>
+public class FlaxEvent<T1, T2>
 {
     private event Action<T1, T2> action = delegate { };
 
@@ -97,10 +94,9 @@ public class Event<T1, T2>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3>
+public class FlaxEvent<T1, T2, T3>
 {
     private event Action<T1, T2, T3> action = delegate { };
 
@@ -125,10 +121,9 @@ public class Event<T1, T2, T3>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4>
+public class FlaxEvent<T1, T2, T3, T4>
 {
     private event Action<T1, T2, T3, T4> action = delegate { };
 
@@ -154,10 +149,9 @@ public class Event<T1, T2, T3, T4>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5>
+public class FlaxEvent<T1, T2, T3, T4, T5>
 {
     private event Action<T1, T2, T3, T4, T5> action = delegate { };
 
@@ -183,10 +177,9 @@ public class Event<T1, T2, T3, T4, T5>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6>
 {
     private event Action<T1, T2, T3, T4, T5, T6> action = delegate { };
 
@@ -212,10 +205,9 @@ public class Event<T1, T2, T3, T4, T5, T6>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7> action = delegate { };
 
@@ -241,10 +233,9 @@ public class Event<T1, T2, T3, T4, T5, T6, T7>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7, T8>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7, T8>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7, T8> action = delegate { };
 
@@ -270,10 +261,9 @@ public class Event<T1, T2, T3, T4, T5, T6, T7, T8>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> action = delegate { };
 
@@ -299,10 +289,9 @@ public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> action = delegate { };
 
@@ -328,10 +317,9 @@ public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> action = delegate { };
 
@@ -357,10 +345,9 @@ public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> action = delegate { };
 
@@ -386,10 +373,9 @@ public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> action = delegate { };
 
@@ -415,10 +401,9 @@ public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> action = delegate { };
 
@@ -444,10 +429,9 @@ public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> action = delegate { };
 
@@ -473,10 +457,9 @@ public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, 
 
 
 /// <summary>
-/// Invokeable Event - this class is an Action/Delegate wrapper with usefull methods for easy Event Subscription and Unsubscription
-/// <para> Allows for the Invokation of events, removing the need for direct script reference or object cache </para>
+/// Custom event implementation that allows subscribing, unsubscribing, and invoking event listeners.
 /// </summary>
-public class Event<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>
+public class FlaxEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>
 {
     private event Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> action = delegate { };
 
